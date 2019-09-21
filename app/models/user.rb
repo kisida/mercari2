95baf397bb55b03c3957461e506bfd628f12c039
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-09-21 11:11:56 +0900

    パスワードの最低文字数を7字にし、英数両方含ませた

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         devise :validatable, password_length: 7..128
+        #  devise :validatable, password_length: 7..128
+        validates_format_of :password, :with => /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, :message => "は７文字以上128字以下の英数両方を含むよう入力してください。"
 end
