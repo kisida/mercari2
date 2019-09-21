f7ed875603a8cd331f9557c6f2cc70e809a8993f
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-09-21 13:49:14 +0900

    全角カラムを追加し、ビューに反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_153841) do
+ActiveRecord::Schema.define(version: 2019_09_21_035119) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_09_20_153841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "lastname"
+    t.string "firstname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
