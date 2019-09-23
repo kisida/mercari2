7c23774e077f76932bea5c0a2da4b942e93949be
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-09-23 19:02:49 +0900

    単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,15 @@ describe User do
      user.valid?
      expect(user.errors[:nickname]).to include("を入力してください")
     end
+    it "is invalid without a email" do
+      user = User.new(nickname: "", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+     end
+     it "is invalid without a password" do
+      user = User.new(nickname: "", email: "", password: "", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+     end
   end
 end
