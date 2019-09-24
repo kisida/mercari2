f3309899fe83f5f8871eeb9333559eb15901bea0
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-09-24 21:17:50 +0900

    SIGN/UPビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   root 'mains#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
-    get 'signup' => 'users/registrations#signup'
-    get "/signup/registration" => "users/registrations#new"
     post "signup/phone_number" => "users/registrations#phone_number"
     post "signup/address" => "users/registrations#address"
     post "signup/credit" => "users/registrations#credit"
