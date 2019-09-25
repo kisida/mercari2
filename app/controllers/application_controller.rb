1a4014b58dc6ab388fc5c2c3c21024384d33d1d3
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-09-25 17:24:25 +0900

    微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production? #basic認証を本番環境のみで利用する下記参照
   protect_from_forgery with: :exception
 
     
 
   private
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname, :firstname, :lastkana, :firstkana, :birthyear, :birthmonth, :birthday, :phone_number])
-  #   # redirect_to 'signup_phone_number_path'
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:lastname, :firstname, :lastkana, :firstkana, :birthyear, :birthmonth, :birthday, :phone_number])
+    # redirect_to 'signup_phone_number_path'
+  end
 
   def production?
     Rails.env.production?
