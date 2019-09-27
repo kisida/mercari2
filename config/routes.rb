b3a9cc41a9701c70ca2d2f285ecdbc784c991613
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-09-27 15:35:35 +0900

    registrationのビュー削除

--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'emailpass' => "users/registrations#emailpass"
-    get "signup/phone_number" => "users/registrations#phone_number"
     get "signup/credit" => "users/registrations#credit"
-
-    
-
     post"signup/card" => "users/registrations#card"
-
-
     end
 
     resources :products
