f14000f99a701526aeba139d5e50a5323f5ff0d7
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-09-28 14:29:01 +0900

    コントローラーにメモ書きまくり

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,25 @@
 class ProductsController < ApplicationController
 
+#未使用
   def index
   end
-
+# 商品詳細画面（関口）
   def show
-    
   end
-
+# 商品出品画面（野口
   def new
   end
-
+# 商品購入確認画面（野口
   def create
   end
-
+# 未使用
   def edit
   end
-
+# 未使用
   def update
   end
-
+# 未使用
   def destroy
   end
+
 end
