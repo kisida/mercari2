c6627b655998c13658a044e822ab891d2470143b
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-10-01 08:22:31 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'emailpass' => "users/registrations#emailpass"
     get "signup/credit" => "users/registrations#credit"
-    post "signup/card" => "users/registrations#card"
+    get "signup/card" => "users/registrations#card"
     end
 
     
