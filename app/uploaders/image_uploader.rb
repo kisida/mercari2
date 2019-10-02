19b15274b1b94198477fdb89aea7e72e2bdc532f
kensaku33 <picow.zoe33@gmail.com>
2019-10-02 12:48:03 +0900

    画像リサイズ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+  #carrierwave内でminimagickを使用する
+  include CarrierWave::MiniMagick
+
+  #リサイズ
+  process resize_to_fill: [300, 300, "Center"]
+
   # 本番環境とローカルで保存先を分ける
   if Rails.env.development? || Rails.env.test?
     storage :file #local
