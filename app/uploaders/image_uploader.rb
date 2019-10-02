eef723a77f0bf996096409b741b3d2063ca87451
kensaku33 <picow.zoe33@gmail.com>
2019-10-02 13:51:56 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,17 +1,16 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  # #carrierwave内でminimagickを使用する
-  # include CarrierWave::MiniMagick
-
-  # #リサイズ
-  # process resize_to_fill: [300, 300]
+  #carrierwave内でminimagickを使用する
+  include CarrierWave::MiniMagick
   
-
+  
   # 本番環境とローカルで保存先を分ける
   if Rails.env.development? || Rails.env.test?
     storage :file #local
+    process resize_to_fill: [300, 300]
   else
     storage :fog #S3
+    process resize_to_fill: [300, 300]
   end
 
 end
