86e940ee1ec76e9368785bc052f167173fd53c5d
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-10-03 18:12:19 +0900

    プロフィールマイページjQuery完成

--- app/assets/javascripts/user_index_tab1.js
+++ app/assets/javascripts/user_index_tab1.js
@@ -1,23 +1,26 @@
-jQuery(function($){
+$(function($){
   $('.top-tab').click(function(){
     $('.is-active').removeClass('is-active');
-    $(this).addClass('is-active');
+    var selectedTab = $('.top-tab').index($(this));
+    $('.top-tab').eq(selectedTab).addClass('is-active');
     $('.is-show').removeClass('is-show');
-// クリックしたタブからインデックス番号を取得
-    const index = $(this).index();
-// クリックしたタブと同じインデックス番号をもつコンテンツを表示
-    $('.top-panel').eq(index).addClass('is-show');
+    // const index = $(this).index();
+    $('.top-panel').eq(selectedTab).addClass('is-show');
   });
 });
 
-// jQuery(function($){
-//   $('.bottom-tab').click(function(){
-//     $('.active').removeClass('active');
-//     $(this).addClass('active');
-//     $('.show').removeClass('show');
-// // クリックしたタブからインデックス番号を取得
-//     const index = $(this).index();
-// // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-//     $('.bottom-panel').eq(index).addClass('show');
-//   });
-// });
+
+jQuery(function($){
+	$('.bottom-tab').click(function(){
+		$('.active-btn').removeClass('active-btn');
+		$(this).addClass('active-btn');
+		$('.show-btn').removeClass('show-btn');
+        // クリックしたタブからインデックス番号を取得
+		const index = $(this).index();
+        // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+		$('.bottom-panel').eq(index).addClass('show-btn');
+	});
+});
+
+//is-active　＝＞　active
+//is-show => show
