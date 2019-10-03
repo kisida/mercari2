d7e6ccd25d7dfd9a737a56f94773beddd5316d39
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-10-03 14:35:41 +0900

    first commit

---
+++ app/assets/javascripts/user_index_tab1.js
@@ -0,0 +1,26 @@
+jQuery(function($){
+  $('.top-tab').click(function(){
+    var parent= $(this).parent()
+    var child =parent.child("") 
+
+    $('.is-active').removeClass('is-active');
+    $(this).addClass('is-active');
+    $('.is-show').removeClass('is-show');
+// クリックしたタブからインデックス番号を取得
+    const index = $(this).index();
+// クリックしたタブと同じインデックス番号をもつコンテンツを表示
+    $('.top-panel').eq(index).addClass('is-show');
+  });
+});
+
+// jQuery(function($){
+//   $('.bottom-tab').click(function(){
+//     $('.active').removeClass('active');
+//     $(this).addClass('active');
+//     $('.show').removeClass('show');
+// // クリックしたタブからインデックス番号を取得
+//     const index = $(this).index();
+// // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+//     $('.bottom-panel').eq(index).addClass('show');
+//   });
+// });
