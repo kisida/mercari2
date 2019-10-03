ec1307415fba6d0b37b0094de4892a5b8c0c56bd
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-10-03 19:25:08 +0900

    first commit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,36 @@
 $(function(){
-  $(".left-btn").on("click", function(){
+  $(".left-btn").click(function(){
     $(".active").removeClass("active");
-
     var clickIndex = $("left-btn").index($(this))
-
     $(".top").eq(clickIndex).addClass("active");
   })
 
 
-  $(".right-btn").on("click", function(){
+  $(".right-btn").click(function(){
     $(".active").removeClass("active");
-
     var clickIndex = $("right-btn").index($(this))
-    
     $(".top").eq(clickIndex).addClass("active");
   })
 });
+
+
+
+// ===================================
+
+var slideCurrent = 0;  // 現在地を示す変数
+
+// アニメーションを実行する独自関数
+var sliding = function(){
+  // slideCurrentが0以下だったら
+  if( slideCurrent < 0 ){
+    slideCurrent = slideNum - 1;
+
+  // slideCurrentがslideNumを超えたら
+  }else if( slideCurrent > slideNum - 1){ // slideCUrrent >= slideNumでも可
+    slideCurrent = 0;
+  }
+
+  $('.slideSet').animate({
+    left: slideCurrent * -slideWidth
+  });
+}
