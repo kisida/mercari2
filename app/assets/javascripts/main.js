b601b9c8956c03e3eadcf7b66ea599262c630704
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-10-04 12:44:56 +0900

    作成開始

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,7 +5,6 @@ $(function(){
     $(".top").eq(clickIndex).addClass("active");
   })
 
-
   $(".right-btn").click(function(){
     $(".active").removeClass("active");
     var clickIndex = $("right-btn").index($(this))
@@ -14,23 +13,7 @@ $(function(){
 });
 
 
+//leftボタンを押した時、写真①が表示される。
+//rightボタンを押した時、写真②が表示される。
+//left-btnを押した時、一回だけ発火するけど次行こうactiveが動かない。
 
-// ===================================
-
-var slideCurrent = 0;  // 現在地を示す変数
-
-// アニメーションを実行する独自関数
-var sliding = function(){
-  // slideCurrentが0以下だったら
-  if( slideCurrent < 0 ){
-    slideCurrent = slideNum - 1;
-
-  // slideCurrentがslideNumを超えたら
-  }else if( slideCurrent > slideNum - 1){ // slideCUrrent >= slideNumでも可
-    slideCurrent = 0;
-  }
-
-  $('.slideSet').animate({
-    left: slideCurrent * -slideWidth
-  });
-}
