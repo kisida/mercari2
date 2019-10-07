21de58f477b211e9cf4f1a63516a8ad37c2e2c5a
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-10-07 15:31:11 +0900

    とりあえず完成

--- config/routes.rb
+++ config/routes.rb
@@ -36,12 +36,12 @@ devise_scope :user do
 
   # collectionはカテゴリー習得用です
   resources :products do
-    get 'buy_edit'
     collection do
-    get 'get_children', defaults: { format: 'json' }
-    get 'get_grand_children', defaults: { format: 'json' }
-    get "products/:id" => "products#update"
-    post 'products/pay' => 'products#pay'
+     get 'get_children', defaults: { format: 'json' }
+     get 'get_grand_children', defaults: { format: 'json' }
+     get "products/:id" => "products#update"
+     get  'buy_edit/:id'=>  'products#buy_edit', as: 'buy_edit'
+     post 'pay/:id' => 'products#pay', as: 'pay'
     end
     member do
       get 'get_children', defaults: { format: 'json' }
