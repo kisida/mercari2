4faebd74e1fe8d61842b4af2b5daaa6410635faa
関口達也 <sekiguchitatsuya@sekiguchitatsuyanoMacBook-Air.local>
2019-10-07 11:53:57 +0900

    スライダーのHTMLが狂ってたので修正

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -13,43 +13,44 @@
 //     $(".top").eq(clickIndex).addClass("active");
 //   })
 // });
+
+
+
 // =========以下関口自作=============================
-var slideCurrent = 0; // 現在地を示す変数
+// var slideCurrent = 0; // 現在地を示す変数
 
-var loop = setInterval(function() {
-  var clone = $(".main-slider-frame li:first").clone(true);
-  $(".main-slider-frame li:first").animate({
-  marginLeft : "-1400px"
-  },{
-  duration : 500,
-  complete : function() {
-      $(".main-slider-frame li:first").remove();
-      clone.clone(true).insertAfter($(".main-slider-frame li:last"));
-    }
-  });
-}, 3000);
+// var loop = setInterval(function() {
+//   var clone = $(".main-slider-frame li:first").clone(true);
+//   $(".main-slider-frame li:first").animate({
+//   marginLeft : "-1400px"
+//   },{
+//   duration : 500,
+//   complete : function() {
+//       $(".main-slider-frame li:first").remove();
+//       clone.clone(true).insertAfter($(".main-slider-frame li:last"));
+//     }
+//   });
+// }, 3000);
 
-////バナーにマウスオーバーしたら止まる===================
-$(function(){
-  $(".main-slider-countainer").hover(function(){
-    clearInterval(loop);                //クリアインターバル解除をどうするか
-  },function(){
-    loop = setInterval(function() {
-      var clone = $(".main-slider-frame li:first").clone(true);
-      $(".main-slider-frame li:first").animate({
-      marginLeft : "-1400px"
-      },{
-      duration : 500,
-      complete : function() {
-      $(".main-slider-frame li:first").remove();
-      clone.clone(true).insertAfter($(".main-slider-frame li:last"));
-        }
-      });
-    }, 3000);//
-  });
-});
-
-
+////バナーにマウスオーバーしたら止まる----------------
+// $(function(){
+//   $(".main-slider-countainer").hover(function(){
+//     clearInterval(loop);                //クリアインターバル解除をどうするか
+//   },function(){
+//     loop = setInterval(function() {
+//       var clone = $(".main-slider-frame li:first").clone(true);
+//       $(".main-slider-frame li:first").animate({
+//       marginLeft : "-1400px"
+//       },{
+//       duration : 500,
+//       complete : function() {
+//       $(".main-slider-frame li:first").remove();
+//       clone.clone(true).insertAfter($(".main-slider-frame li:last"));
+//         }
+//       });
+//     }, 3000);//
+//   });
+// });
 //ボタンを押したら進む(一応動いている)---------
 // $(function(){
 //   $('.slider-prev').click(function(){
@@ -64,23 +65,15 @@ $(function(){
 //     });
 //   });
 // });
-// ======================================
-  // 前へボタンが押されたとき
-  // $('.slider-prev').click(function(){
-  //   slideCurrent--;
-  //   loop();
-  // });
-  // // 次へボタンが押されたとき
-  // $('.slider-next').click(function(){
-  //   slideCurrent++;
-  //   loop();
-  // });
 
-  // $(document).ready(function(){
-  //   $('.main-slider-frame').slick({
-  //     autoplay:true,
-  //     dots:false,
-  //     arrows: false,
-  //     pauseOnHover: true,
-  //   });
-  // });
+// ===========以下Slick slider===========
+
+
+  $(document).ready(function(){
+    $('.top-image').slick({
+      autoplay:true,
+      dots:false,
+      arrows: false,
+      pauseOnHover: true,
+    });
+  });
