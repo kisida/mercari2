bcc752fc9a3da516d71dd369c7b6badce2accab8
nakajimakouta1002 <kouta.tennis.1002@icloud.com>
2019-10-07 17:56:52 +0900

    完璧

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -8,7 +8,6 @@ class MainsController < ApplicationController
 
     @ladys = Item.where(category_id:1...89).order("id DESC").limit(10)
     @mens = Item.where(category_id:90...194).order("id DESC").limit(10)
-
   end
 
   def search
